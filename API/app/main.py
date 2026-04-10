6377853a0ee81d40cea501c3c9dbf8ed2a94d4e2
feneau <feneauemma@gmail.com>
2026-04-10 18:43:52 +0200

    fix: explicitly allow OPTIONS in CORS

--- API/app/main.py
+++ API/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
         "http://localhost:3000",
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
